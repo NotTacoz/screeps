6af636e94a4facde35dcda711e8d352590e98b7d
NotTacoz <tacozpvp@gmail.com>
2021-03-19 14:17:29 +0800

    Basic Consistent (Working) Code

--- main.js
+++ main.js
@@ -36,16 +36,22 @@ module.exports.loop = function() {
         // spawning creeps auto
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+        var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
         if (harvesters.length < 2) {
             var newName = 'Harvester' + Game.time;
             console.log('Spawning new harvester: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: 'harvester' } });
         }
-        if (upgraders.length < 1) {
+        if (upgraders.length < 2) {
             var newName = 'Upgrader' + Game.time;
             console.log('Spawning new upgrader: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: 'upgrader' } });
+        }
+        if (builders.length < 2) {
+            var newName = 'Builder' + Game.time;
+            console.log('Spawning new builder: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: 'builder' } });
         }
 
         if (Game.spawns['Spawn1'].spawning) {
