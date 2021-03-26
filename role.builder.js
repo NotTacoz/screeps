23f2f78596fe4e414ee2b613f786e076a70ac835
NotTacoz <tacozpvp@gmail.com>
2021-03-26 10:34:38 +0800

    yay

--- role.builder.js
+++ role.builder.js
@@ -1,25 +1,23 @@
-var roleBuilder = {
+var roleBuilder = { // builder role poggers
 
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+        if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) { // if the creep is not empty
             creep.memory.building = false;
-            creep.say('🔄 harvest');
+            creep.say('🔄 harvest'); // do the harvest thing and broadcast it
         }
-        if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+        if (!creep.memory.building && creep.store.getFreeCapacity() == 0) { // if the creep is full
             creep.memory.building = true;
-            creep.say('🚧 build');
+            creep.say('🚧 build'); // do the building thing and broadcast it
         }
 
         if (creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            console.log("creep.build(targets[0])");
-            if (targets.length) {
-                if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES); // find construction sitess
+            //console.log("creep.build(targets[0])"); //temporary file lol
+            if (targets.length > 0) { // more then one construction site
+                if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) { // try to build
                     creep.moveTo(targets[0]), { visualizePathStyle: { stroke: '#ffffff' } };
-                } else if (creep.build(targets[0]) == ERR_INVALID_TARGET) {
-                    console.log("a");
                 }
             } else if (creep.build(targets[0]) == ERR_INVALID_TARGET) {
                 console.log("a");
--- role.harvester.js
+++ role.harvester.js
@@ -1,25 +1,27 @@
-var roleHarvester = {
+var roleHarvester = { // role harvester
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if (creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+        if (creep.store.getFreeCapacity() > 0) { // is the creep full?
+            var sources = creep.room.find(FIND_SOURCES); // find sources
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) { // try to harvest, if the source returns the not in range error then,
+                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } }); // move to source whilst drawing path
             }
-        } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+        } else { // else
+            var targets = creep.room.find(FIND_STRUCTURES, { // find structres
+                filter: (structure) => { // if the structure is 
+                    return (structure.structureType == STRUCTURE_EXTENSION || // extension
+                            structure.structureType == STRUCTURE_SPAWN || // spawn
+                            structure.structureType == STRUCTURE_TOWER) && // or tower
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0; // and these structures has to not be full
                 }
             });
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
+            if (targets.length > 0) { // if there are more then one targets
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) { // fill
+                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } }); // yes
                 }
+            } else {
+                // i want to add interchangable roles here
             }
         }
     }
